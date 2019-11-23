9530e7221a70f41f5a9fa022e6b74a1ed4f00f1d
vutivi <201141790@student.uj.ac.za>
2019-11-23 08:54:54 +0200

    chore: rover spec/logic

--- spec/surface_spec.rb
+++ spec/surface_spec.rb
@@ -4,11 +4,11 @@ describe Surface do
   let(:surface) { Surface.new(10, 20) }
 
   describe 'initialize' do
-    fit 'returns correct surface length' do
+    it 'returns correct surface length' do
       expect(surface.height).to eq(10)
     end
 
-    fit 'returns correct surface width' do
+    it 'returns correct surface width' do
       expect(surface.width).to eq(20)
     end
   end
